2cb058a825a51cbf668720b5be012290f0dff853
zhouwei <zhouwei@live.it>
2017-04-01 02:02:23 +0800

    添加config示例

--- webapp/config.example.py
+++ webapp/config.example.py
@@ -8,7 +8,9 @@ class Config:
 class DevConfig:
     basedir = path.abspath(path.dirname(__file__))
     DEBUG = True
-    SQLALCHEMY_DATABASE_URI = "mysql+pymysql://simple_dev:simple_dev@192.168.5.7:3306/simple_task_test?charset=utf8mb4"
-    SECRET_KEY = 'hard to guess string abc heheda'
+    SQLALCHEMY_DATABASE_URI = 1
+    SECRET_KEY = 1
     SQLALCHEMY_ECHO = False
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+    WBAppKey = 1
+    WBAppSecret = 1
